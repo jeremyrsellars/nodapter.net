9978d8ccb647d8578b4eb7a630dad85e16aceff5
Jeremy Sellars <jeremy.sellars@softekinc.com>
2012-11-30 14:29:39 -0600

    Added ChecksumWork

---
+++ Nodapter.Core/Commands/ChecksumWork.cs
@@ -0,0 +1,21 @@
+﻿namespace Nodapter.Core.Commands
+{
+   public class ChecksumWork : Nodapter.Core.ISyncWork
+   {
+      readonly string argument;
+
+      public ChecksumWork(string argument)
+      {
+         this.argument = argument;
+      }
+
+      string ISyncWork.Run(IWorkContext context)
+      {
+         System.Threading.Thread.Sleep(1500);
+         int sum = 0;
+         foreach(int c in argument)
+            sum ^= c;
+         return sum.ToString();
+      }
+   }
+}
